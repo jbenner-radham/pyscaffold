3c396dc70203bbbd0933a692b5178e6fbd3c8c3b
James Benner <james_benner@abercrombie.com>
2017-04-08 19:49:31 -0400

    Moar dots!

--- pyscaffold/__main__.py
+++ pyscaffold/__main__.py
@@ -4,7 +4,7 @@ import os.path
 dir = '__scaffold__'
 files = ['MANIFEST.in', 'README.rst', 'setup.cfg', 'setup.py']
 
-print(f'Hello world! Scaffolding out to {dir}.')
+print(f'Hello world! Scaffolding out to {dir}...')
 Path(dir).mkdir(exist_ok=True)
 
 for file in files:
