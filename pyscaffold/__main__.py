69d2bed200cfb9bab0ffdc5a63ffad682cc57a26
James Benner <james_benner@abercrombie.com>
2017-04-08 17:21:52 -0400

    Adding initial "main" file.

--- pyscaffold/__main__.py
+++ pyscaffold/__main__.py
@@ -1,1 +1,12 @@
-print('Hello world!')
+from pathlib import Path
+import os.path
+
+dir = '__scaffold__'
+files = ['MANIFEST.in', 'README.rst', 'setup.cfg', 'setup.py']
+
+print(f'Hello world! Scaffolding out to {dir}.')
+Path(dir).mkdir(exist_ok=True)
+
+for file in files:
+    filepath = os.path.join(dir, file)
+    Path(filepath).touch()
